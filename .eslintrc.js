d0359a4953210b94db4ed0096f391c528474701b
Tyson Andre <tandre@themeetgroup.com>
2018-04-05 11:23:15 -0700

    Override eslintrc for this project

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+// TODO: Decide on a style guide?
+module.exports = {
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    // Not sure if this is exact
+    "ecmaVersion": 6,
+    "sourceType": "script"
+  },
+  "env": {
+    // "browser": true,
+    "node": true,
+    "mocha": true
+  }
+}
